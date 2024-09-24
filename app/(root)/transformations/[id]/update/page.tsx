fb2cacdde7086aa67150c4bc60fc2ad5ca00b72f
Swejan17 <swejanchowdary01072004@gmail.com>
2024-09-24 12:56:28 +0530

    final commit

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const UpdateTransormationPage = ({params}:{params:{id:String}}) => {
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TranformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { getImageById } from "@/lib/actions/image.actions";
+
+const Page = async ({ params: { id } }: SearchParamProps) => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
   return (
-    <div>
-      UpdateTransormationPage
-    </div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default UpdateTransormationPage
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={user._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
